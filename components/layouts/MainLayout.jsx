edf70fbf0c5817bf4d822026644ba553969cf9cf
Juan Camilo Muñoz Zuleta <juancmz98@gmail.com>
2022-05-23 18:46:25 -0500

    Add: Main Layout

---
+++ components/layouts/MainLayout.jsx
@@ -0,0 +1,26 @@
+import Head from 'next/head'
+import Navbar from '../Navbar'
+
+import styles from './MainLayout.module.css'
+
+const MainLayout = ({ children }) => {
+  return (
+    <div className={styles.container}>
+
+      <Head>
+        <title>Home - Juan Camilo</title>
+        <meta name="description" content="Home Page" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Navbar />
+
+      <main className={styles.main}>
+        { children }
+      </main>
+
+    </div>
+  )
+}
+
+export default MainLayout
--- pages/about.jsx
+++ pages/about.jsx
@@ -1,33 +1,18 @@
-import Head from 'next/head'
 import Link from 'next/link'
-import Navbar from '../components/Navbar'
-import styles from '../styles/Home.module.css'
+import MainLayout from '../components/layouts/MainLayout'
 
 export default function AboutPage() {
   return (
-    <div className={styles.container}>
+    <MainLayout>
+      <h1>About Page</h1>
+      <h1 className="title">
+        Ir a <Link href="/"><a>Home</a></Link>
+      </h1>
 
-      <Head>
-        <title>About - Juan Camilo</title>
-        <meta name="description" content="About Page" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <Navbar />
-
-
-      <main className={styles.main}>
-        <h1>About Page</h1>
-        <h1 className={styles.title}>
-          Ir a <Link href="/"><a>Home</a></Link>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/about.jsx</code>
-        </p>
-      </main>
-
-    </div>
+      <p className="description">
+        Get started by editing{' '}
+        <code className="code">pages/about.jsx</code>
+      </p>
+    </MainLayout>
   )
 }
