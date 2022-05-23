a6a939a78d7b66a831e4b8119a8bbdb232a339fe
Juan Camilo Muñoz Zuleta <juancmz98@gmail.com>
2022-05-23 16:58:31 -0500

    Add: ActiveLink component and contact page

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,11 +1,13 @@
-import Link from "next/link"
+import ActiveLink from './ActiveLink'
+
+import styles from './Navbar.module.css'
 
 const Navbar = () => {
   return (
-    <nav>
-      <Link href="/"><a>Home</a></Link>
-      <Link href="/about"><a>About</a></Link>
-      <Link href="/contact"><a>Contact</a></Link>
+    <nav className={ styles['menu-container'] }>
+      <ActiveLink text="Home" href="/" />
+      <ActiveLink text="About" href="/about" />
+      <ActiveLink text="Contact" href="/contact" />
     </nav>
   )
 }
