e0f2760bfd9a5ced3b3f4586dfe5e7124e6e070b
Juan Camilo Muñoz Zuleta <juancmz98@gmail.com>
2022-05-24 18:38:53 -0500

    Add: Pricing page

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,12 +2,33 @@ import ActiveLink from './ActiveLink'
 
 import styles from './Navbar.module.css'
 
+const menuItems = [
+  {
+      text: 'Home',
+      href: '/'
+  },
+  {
+      text: 'About',
+      href: '/about'
+  },
+  {
+      text: 'Contact',
+      href: '/contact'
+  },
+  {
+      text: 'Pricing',
+      href: '/pricing'
+  },
+]
+
 const Navbar = () => {
   return (
     <nav className={ styles['menu-container'] }>
-      <ActiveLink text="Home" href="/" />
-      <ActiveLink text="About" href="/about" />
-      <ActiveLink text="Contact" href="/contact" />
+      {
+        menuItems.map(({text, href}) => (
+          <ActiveLink key={href} text={text} href={href} />
+        ))
+      }
     </nav>
   )
 }
